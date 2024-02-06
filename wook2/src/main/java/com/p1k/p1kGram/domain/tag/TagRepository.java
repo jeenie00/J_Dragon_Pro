5e6bb942b049123f45f1fca2f5b4e5426976473d
leejeongseop <155722906+leejeongseop@users.noreply.github.com>
2024-02-06 18:09:04 +0900

    Delete wook2 directory

--- wook2/src/main/java/com/p1k/p1kGram/domain/tag/TagRepository.java
+++
@@ -1,8 +0,0 @@
-package com.p1k.p1kGram.domain.tag;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-// Tag, integer → jpaRepository를 그대로 따옴
-public interface TagRepository extends JpaRepository<Tag, Integer>{
-
-}
--- wook2/wook_jsp/resources/src/main/java/com/p1k/p1kGram/service/PostService.java
+++
@@ -1,5 +0,0 @@
-package com.p1k.p1kGram.service;
-
-public class PostService {
-
-}
