c8ce50718c5c5d368743d5041432e3a4c972e26b
leejeongseop <155722906+leejeongseop@users.noreply.github.com>
2024-02-06 18:08:26 +0900

    Delete ljs directory

--- ljs/java/com/p1k/p1kGram/domain/user/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.p1k.p1kGram.domain.user;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Integer>{
-
-	User findByUsername(String username);}
